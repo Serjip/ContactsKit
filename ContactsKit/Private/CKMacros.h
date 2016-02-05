44abce34eeba6f8d41cfbc9eaf3859e581b77bd7
Sergey Popov <serji.popov@gmail.com>
2016-02-06 00:29:27 +0300

    Added macros for messenger

--- ContactsKit/Private/CKMacros.h
+++ ContactsKit/Private/CKMacros.h
@@ -21,10 +21,25 @@
 #define kABAddressCountryKey        (__bridge NSString *)kABPersonAddressCountryKey
 #define kABAddressCountryCodeKey    (__bridge NSString *)kABPersonAddressCountryCodeKey
 // Social profile
-#define kABSocialProfileURLKey              (__bridge_transfer NSString *)kABPersonSocialProfileURLKey
-#define kABSocialProfileUsernameKey         (__bridge_transfer NSString *)kABPersonSocialProfileUsernameKey
-#define kABSocialProfileUserIdentifierKey   (__bridge_transfer NSString *)kABPersonSocialProfileUserIdentifierKey
-#define kABSocialProfileServiceKey          (__bridge_transfer NSString *)kABPersonSocialProfileServiceKey
+#define kABSocialProfileURLKey              (__bridge NSString *)kABPersonSocialProfileURLKey
+#define kABSocialProfileUsernameKey         (__bridge NSString *)kABPersonSocialProfileUsernameKey
+#define kABSocialProfileUserIdentifierKey   (__bridge NSString *)kABPersonSocialProfileUserIdentifierKey
+#define kABSocialProfileServiceKey          (__bridge NSString *)kABPersonSocialProfileServiceKey
+
+// Messenger
+#define kABInstantMessageUsernameKey    (__bridge NSString *)kABPersonInstantMessageUsernameKey
+#define kABInstantMessageServiceKey     (__bridge NSString *)kABPersonInstantMessageServiceKey
+
+#define kABInstantMessageServiceAIM			(__bridge NSString *)kABPersonInstantMessageServiceAIM          // AIM
+#define kABInstantMessageServiceFacebook	(__bridge NSString *)kABPersonInstantMessageServiceFacebook		// Facebook
+#define kABInstantMessageServiceGaduGadu	(__bridge NSString *)kABPersonInstantMessageServiceGaduGadu		// Gadu-Gadu
+#define kABInstantMessageServiceGoogleTalk	(__bridge NSString *)kABPersonInstantMessageServiceGoogleTalk	// Google Talk
+#define kABInstantMessageServiceICQ			(__bridge NSString *)kABPersonInstantMessageServiceICQ          // ICQ
+#define kABInstantMessageServiceJabber		(__bridge NSString *)kABPersonInstantMessageServiceJabber		// Jabber
+#define kABInstantMessageServiceMSN			(__bridge NSString *)kABPersonInstantMessageServiceMSN          // MSN
+#define kABInstantMessageServiceQQ			(__bridge NSString *)kABPersonInstantMessageServiceQQ           // QQ
+#define kABInstantMessageServiceSkype		(__bridge NSString *)kABPersonInstantMessageServiceSkype        // Skype
+#define kABInstantMessageServiceYahoo		(__bridge NSString *)kABPersonInstantMessageServiceYahoo        // Yahoo!
 
 #endif
 
