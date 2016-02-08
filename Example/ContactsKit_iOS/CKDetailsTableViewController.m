164431eaf7ce642cd56a801e73d59fee2b8992de
Sergey Popov <serji.popov@gmail.com>
2016-02-08 21:00:41 +0300

    Added properties enumerator.

--- Example/ContactsKit_iOS/CKDetailsTableViewController.m
+++ Example/ContactsKit_iOS/CKDetailsTableViewController.m
@@ -46,21 +46,10 @@
     
     NSMutableDictionary *map = [[NSMutableDictionary alloc] init];
     
-    unsigned int count = 0;
-    objc_property_t *properties = class_copyPropertyList([CKContact class], &count);
-    
-    for (unsigned int i = 0; i < count; i++)
-    {
-        objc_property_t p = properties[i];
-        NSString *name =  @(property_getName(p));
-        NSString *attributes = @(property_getAttributes(p));
-        NSString *type = [attributes componentsSeparatedByString:@","].firstObject;
-        
-        NSLog(@"%@ %@",name, type);
+    [self ck_enumeratePropertiesOfClass:[CKContact class] usingBlock:^(NSString *name, __unsafe_unretained Class type) {
         
         
-    }
-    free(properties);
+    }];
 }
 
 #pragma mark - UITableViewDataSource
@@ -81,5 +70,30 @@
 {
     
 }
+
+#pragma mark - Private
+
+- (void)ck_enumeratePropertiesOfClass:(Class)aClass usingBlock:(void (^) (NSString *name, Class type))block
+{
+    unsigned int count = 0;
+    objc_property_t *properties = class_copyPropertyList(aClass, &count);
+    
+    for (unsigned int i = 0; i < count; i++)
+    {
+        objc_property_t p = properties[i];
+        NSString *name =  @(property_getName(p));
+        NSString *attributes = @(property_getAttributes(p));
+        NSString *type = [attributes componentsSeparatedByString:@","].firstObject;
+        type = [type substringFromIndex:1];
+        
+        if ([type hasPrefix:@"@"])
+        {
+            type = [type substringWithRange:NSMakeRange(2, type.length - 3)];
+            block(name, NSClassFromString(type));
+        }
+    }
+    free(properties);
+}
+
 
 @end
